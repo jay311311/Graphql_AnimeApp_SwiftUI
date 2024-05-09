9b3d936751757ab2b2b3ea1e071b0b867c761f92
jooeun kim <jay311311@gmail.com>
2024-05-09 12:53:15 -0400

    feat. MainView List의 UI 변경 - AnimeList ui 변경 - DetailView 생성

---
+++ Graphql_AnimeApp_SwiftUI/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailView.swift
+//  Graphql_AnimeApp_SwiftUI
+//
+//  Created by Jooeun Kim on 2024-05-09.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
--- Graphql_AnimeApp_SwiftUI/MainView/MainView.swift
+++ Graphql_AnimeApp_SwiftUI/MainView/MainView.swift
@@ -32,31 +32,42 @@ struct MainView: View {
     var AnimeList: some View {
         ScrollView{
             ForEach(viewModel.animes, id: \.self) { anime in
-                if !anime.synonyms.isEmpty {
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.red)
-                            .frame(width: 250, height: 100)
-                        VStack {
+                if !anime.synonyms.isEmpty && anime.bannerImage != nil {
+                    Group {
+                        LazyVStack(alignment: .center){
                             AsyncImage(url: URL(string: anime.bannerImage ?? "")) { image in
                                 image.resizable()
-                                Text("\(anime.synonyms[0] ?? "")")
-                                    .font(.system(size: 13, weight: .bold))
                             } placeholder: {
                                 PlaceHolderView()
                             }
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 250, height: 100)
+                            VStack(spacing: 0) {
+                                Text("\(anime.synonyms[0] ?? "")")
+                                    .font(.system(size: 12, weight: .medium))
+                                HStack() {
+                                    ForEach(anime.genres.indices, id: \.self) { index in
+                                        if index < 4 {
+                                            Text(anime.genres[index] ?? "")
+                                                .font(.system(size: 9, weight: .light))
+                                        }
+                                    }
+                                }
+                                Spacer()
+                            }
+                            .frame(height: 40)
                         }
+                        .frame(width: 250, height: 140)
+                        .background(.gray)
+                        .foregroundStyle(.white)
+                        .clipShape( RoundedRectangle(cornerRadius: 8) )
                     }
-                    .presentationCornerRadius(5)
-                    .foregroundStyle(.blue)
                 }
             }
         }
     }
 }
-
-#Preview {
-    MainView(viewModel: .init(service: AnimeNetworkService(networking: AnimeNetwork())))
-}
+    
+    #Preview {
+        MainView(viewModel: .init(service: AnimeNetworkService(networking: AnimeNetwork())))
+    }
